949d4a37ad1587882ccf2254a4b092bc6ebe97cb
Doniyor Botirov <Rayden33@mail.ru>
2024-10-16 00:47:25 +0500

    Complete 0.0.1 version and add progress bar

--- src/mosaipic.cpp
+++ src/mosaipic.cpp
@@ -11,9 +11,13 @@ using namespace cv;
 
 struct Config
 {
+    string mainImagePath;
+    string smallImagesDirPath;
+    string resultImagePath;
     int mainImageScale;
     int mosaicBlockWidth;
-    int mosaicBlockHeight; 
+    int mosaicBlockHeight;
+    int progressBarWidth;
 };
 
 /**
@@ -112,9 +116,48 @@ bool loadIniConfiguration(const string& filename, Config& config)
         return false;
     }
 
-    config.mainImageScale = stoi(ini.GetValue("settings", "mosaic_size_scale", "1"));
-    config.mosaicBlockWidth = stoi(ini.GetValue("settings", "mosaic_block_width", "32"));
-    config.mosaicBlockHeight = stoi(ini.GetValue("settings", "mosaic_block_height", "32"));
+    config.mainImagePath = ini.GetValue("settings","main_image_path", "main.jpg");
+    config.smallImagesDirPath = ini.GetValue("settings","small_images_dir_path", "parts");
+    config.resultImagePath = ini.GetValue("settings","result_image_path", "result.jpg");
+    config.mainImageScale = stoi(ini.GetValue("sizes", "mosaic_size_scale", "1"));
+    config.mosaicBlockWidth = stoi(ini.GetValue("sizes", "mosaic_block_width", "32"));
+    config.mosaicBlockHeight = stoi(ini.GetValue("sizes", "mosaic_block_height", "32"));
+    config.progressBarWidth = stoi(ini.GetValue("sizes", "progress_bar_width", "50"));
+    
+    return true;
+}
+
+int countFilesInDirectory(const std::string& path) {
+    int fileCount = 0;
+
+    if (!filesystem::exists(path) || !filesystem::is_directory(path)) {
+        std::cerr << "The path does not exist or is not a folder: " << path << std::endl;
+        return -1;
+    }
+
+    for (const auto& entry : filesystem::directory_iterator(path)) {
+        if (filesystem::is_regular_file(entry.status())) {
+            fileCount++;
+        }
+    }
+    return fileCount;
+}
+
+void showProgress(int progress, int total, int width, const string& detail = "")
+{
+    int pos = width * progress / total;
+    const char spinner[] = {'|', '/', '-', '\\'};
+
+    cout << "[";
+    for(int i=0; i<width; i++)
+    {
+        if(i < pos) cout << "=";
+        else if (i == pos) cout << ">";
+        else cout << " ";
+    }
+    cout << "] " << int(progress * 100.0 / total) << "% ";
+    std::cout << spinner[progress % 4] << " " << detail << "\r";
+    cout.flush();
 }
 
 
@@ -124,5 +167,82 @@ int main()
     if(!loadIniConfiguration("config.ini", config))
         return 1;
 
-    
+    string mainImagePath = config.mainImagePath;
+    string smallImagesDirPath = config.smallImagesDirPath;
+    string resultImagePath = config.resultImagePath;
+
+    int smallImagesCount = countFilesInDirectory(config.smallImagesDirPath);
+    int mainImageScale = config.mainImageScale;
+    int blockWidth = config.mosaicBlockWidth;
+    int blockHeight = config.mosaicBlockHeight;
+    int progressBarWidth = config.progressBarWidth;
+    int counter = 0;
+    int numCols;
+    int bestMatchIndex;
+    int row;
+    int col;
+    int curWidth;
+    int curHeight;
+
+    vector<Mat> smallImages;
+    vector<Scalar> averageColors;
+    vector<Mat> mosaicBlocks;
+
+    Scalar avgColor;
+
+    Mat mainImage = loadImage(mainImagePath);
+    Mat smallImage;
+    Mat resultImage;
+
+    if(mainImage.empty())
+    {
+        cerr<<"Could not open the main image: "<< mainImagePath;
+        return -1;
+    }
+
+    resize(mainImage, mainImage, Size(mainImage.cols * mainImageScale, mainImage.rows * mainImageScale), INTER_LINEAR);
+    mosaicBlocks = splitImageIntoBlocks(mainImage, blockWidth, blockHeight);
+    resultImage = Mat(mainImage.size(), mainImage.type());
+
+    for(const auto& entry : filesystem::directory_iterator(smallImagesDirPath))
+    {
+        smallImage = loadImage(entry.path().string());
+        if(!smallImage.empty())
+        {
+            resize(smallImage, smallImage, Size(blockWidth, blockHeight));
+            smallImages.push_back(smallImage);
+            averageColors.push_back(computeAverageColor(smallImage));
+            showProgress(counter++, smallImagesCount, progressBarWidth, entry.path().filename().string());
+        }
+    }
+
+    numCols = mainImage.cols / blockWidth;
+
+    for(int i=0; i<mosaicBlocks.size(); i++)
+    {
+        avgColor = computeAverageColor(mosaicBlocks[i]);
+        bestMatchIndex = findBestMatch(avgColor, averageColors);
+
+        row = (i/numCols) * blockHeight;
+        col = (i%numCols) * blockWidth;
+
+        if(bestMatchIndex < 0)
+        {
+            cerr<<"Not found best index";
+            return -1;
+        }
+
+        curWidth = min(blockWidth, mainImage.cols - col);
+        curHeight = min(blockHeight, mainImage.rows - row);
+
+        if(col + blockWidth <= resultImage.cols && row + blockHeight <= resultImage.rows)
+        {
+            smallImages[bestMatchIndex].copyTo(resultImage(Rect(col, row, curWidth, curHeight)));
+        }
+    }
+
+    imwrite(resultImagePath, resultImage);
+    cout<< "Photomosaic created";
+
+    return 0;
 }
